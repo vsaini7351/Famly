e3da2fa7d1dd60aa7dea5c57298cc2d2f327ec7e
Guddan-Deepak <deepakdesh565@gmail.com>
2025-10-05 19:57:51 +0530

    Implement layout structure with Header and Footer components; add Home page and update routing; include background images for Login and Home pages.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,17 +1,18 @@
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {Route,createBrowserRouter,createRoutesFromElements,RouterProvider} from 'react-router-dom'
 import { AuthProvider } from './utils/authContext.jsx'
-import AuthCard from './components/Login/Login.jsx'
+import Home from './pages/Home/HomePage.jsx'
 import AuthPage from './pages/auth/Login.jsx'
 
 
 const router=createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<AuthPage/>} errorElement>
+    <Route path='/' element={<App />} errorElement>
 
+      <Route index element={<Home/>} />
+      <Route path='auth' element={<AuthPage/>} />
     </Route>
   )
 )
--- Frontend/src/pages/Home/HomePage.jsx
+++ Frontend/src/pages/Home/HomePage.jsx
@@ -1,1 +1,30 @@
 //it include create family options
+//it include create family options
+import hero from "../../assets/home-image.jpg";
+
+const Home = () => {
+  return (
+    <div
+      className="relative min-h-[90vh] flex items-center justify-center bg-cover bg-center"
+        style={{ backgroundImage: `url(${hero})` }}
+    >
+      <div className="bg-white/40 backdrop-blur-md rounded-2xl p-8 max-w-2xl shadow-lg">
+    <h1 className="text-4xl font-bold text-purple-700 mb-4">
+      Welcome to FAMLY 👨‍👩‍👧‍👦
+    </h1>
+    <p className="text-purple-700 mb-6">
+      Preserve and share your family’s precious memories with love.  
+      Create a family circle, add members, and keep your memories alive forever.
+    </p>
+    <a
+      href="/auth"
+      className="bg-gradient-to-r from-purple-500 to-purple-700 text-white px-6 py-3 rounded-md font-semibold hover:opacity-90 transition"
+    >
+      Get Started
+    </a>
+  </div>
+    </div>
+  );
+};
+
+export default Home;
